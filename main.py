24393737da4abb09e5a57a63c262ab87a8d1a1d0
Aya A <96552987+aaitb@users.noreply.github.com>
2022-01-05 23:00:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,4 +8,15 @@ def judge_leap_year(year):
     else:
         return False
 
-#
+
+# This will return the number of days in every month
+# Format (1 = January, etc.)
+def month_days(month, leap_year):
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2 and leap_year:
+        return 29
+    elif month == 2 and (not leap_year):
+        return 28
